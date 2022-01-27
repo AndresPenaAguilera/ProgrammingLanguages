ec343baaea4c13c3f94b64326d8a061650402432
Andrés Peña <p.andres01@hotmail.com>
2022-01-27 08:18:23 -0500

    Entendiendo EntityFrameworkCore

---
+++ C#-React/NetCore/leerData/AppVentaCursosContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace leerData
+{
+    public class AppVentaCursosContext : DbContext
+    {
+        private const string connectionString =@"Data Source=GTH-93\SQLEXPRESS; Initial Catalog=CursosOnline;User ID=sa;Password=Sinco123;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Curso> Curso {get; set;}
+    }
+}
