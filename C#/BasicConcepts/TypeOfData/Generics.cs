a968bc2548822602885c4ce9ea7a55423e5179d0
Andrés Peña <p.andres01@hotmail.com>
2022-02-03 18:28:31 -0500

    Add generics

---
+++ C#/BasicConcepts/TypeOfData/Generics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConcepts.TypeOfData
+{
+    public class Generics<T>
+    {
+        private int i = 0;
+        private T[] matriz;
+
+        public Generics(int elements) 
+        {
+            matriz = new T[elements];
+        }
+
+        public void AddElement(T element) 
+        {
+            matriz[i] = element;
+            i++;
+        }
+
+        public T GetElement(int position) 
+        {
+            return matriz[position];
+        }
+    }
+
+
+    public class Student 
+    {
+        double Score { get; set; }
+
+        public Student(double score) 
+        {
+            Score = score;
+        }
+    }
+}
---
+++ C#/TestProject1/TypeOfData/TestGenerics.cs
@@ -0,0 +1,32 @@
+﻿using BasicConcepts.TypeOfData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1.TypeOfData
+{
+    [TestClass]
+    public class TestGenerics
+    {
+        [TestMethod]
+        public void Must_Generics_ReturnType_Alumno()
+        {
+            Generics<Student> generics = new Generics<Student>(2);
+            generics.AddElement(new Student(1));
+            generics.AddElement(new Student(1.5));
+
+            var actual = generics.GetElement(0);
+            Assert.AreEqual(typeof(Student), actual.GetType());
+        }
+
+        [TestMethod]
+        public void Must_Generics_ReturnType_String()
+        {
+            Generics<string> generics = new Generics<string>(2);
+            generics.AddElement("Juan");
+            generics.AddElement("Otro");
+
+            var actual = generics.GetElement(0);
+            Assert.AreEqual(typeof(string), actual.GetType());
+            Assert.AreEqual("Juan", actual);
+        }
+    }
+}
