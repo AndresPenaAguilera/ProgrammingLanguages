95285980490ad39b81180b150329bed01e5cc57f
Andrés Peña <p.andres01@hotmail.com>
2022-02-04 09:22:05 -0500

    Add Collection Stack

---
+++ C#/BasicConcepts/TypeOfData/CollectionStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConcepts.TypeOfData
+{
+    public class CollectionStack
+    {
+        public Stack<double> stack;
+        public CollectionStack() 
+        {
+            stack = new Stack<double>();
+        }
+
+        public void AddElement(double value) 
+        {
+            stack.Push(value);
+        }
+        public string GetElement() 
+        {
+            int i = 0;
+            string values="";
+            foreach (double element in stack) 
+            {
+                values += string.Format("index {0} value {1} -", i++, element);
+            }
+            return values;
+        }
+
+        
+    }
+}
---
+++ C#/TestProject1/TypeOfData/TestCollectionStack.cs
@@ -0,0 +1,22 @@
+﻿using BasicConcepts.TypeOfData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1.TypeOfData
+{
+    [TestClass]
+    public class TestCollectionStack
+    {
+
+        [TestMethod]
+        public void Must_CollectionStack_ReturnValuesWithPosition()
+        {
+            CollectionStack collectionStack = new CollectionStack();
+            
+            collectionStack.AddElement(1.1);
+            Assert.AreEqual("index 0 value 1.1 -", collectionStack.GetElement());
+
+            collectionStack.AddElement(2);
+            Assert.AreEqual("index 0 value 2 -index 1 value 1.1 -", collectionStack.GetElement());
+        }
+    }
+}
