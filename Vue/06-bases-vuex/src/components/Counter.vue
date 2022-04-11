64a8a5c76b4b3bc1183f173ed9e061b3695ba5ee
Andrés Peña <p.andres01@hotmail.com>
2022-04-11 14:11:21 -0500

    add vuex

---
+++ Vue/06-bases-vuex/src/components/Counter.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>Counter - vuex</h1>
+  <h2> Direct access: {{ $store.state.count }}</h2>
+  <h2> Computed: {{ countComputed }}</h2>
+  <h2> mapState: {{ count }}</h2>
+  <h2> mapState other value: {{ lastMutation }}</h2>
+  <h2> mapState object: {{ counter }}</h2>
+  
+<button @click="increment">increment +1</button>
+<button @click="incrementBy"> +5</button>
+<button @click="incrementRandomInt"> random</button>
+
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+    computed:{
+        countComputed(){
+            return this.$store.state.count
+        },
+
+        ...mapState(['count','lastMutation']),
+
+        ...mapState({
+            counter: state => state.count
+        })
+    }, 
+    
+    methods:{
+
+        increment(){
+            this.$store.commit('increment')
+        },
+
+        incrementBy(){
+            this.$store.commit('incrementBy', 5)
+        },
+        incrementRandomInt(){
+            this.$store.dispatch('incrementRandomInt')
+        }
+    }
+}
+</script>
+
